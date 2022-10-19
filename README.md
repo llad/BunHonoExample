feaeb5a09fec99388295d994caab77d0873bb30d
Mark Woodall <mwdll@icloud.com>
2022-10-19 02:40:40 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bun
+Start editing the <code>index.ts</code> file. We also, provided you with a <code>/templates/</code> folder.
+
+You can run the <code>index.ts</code> file by typing `bun index.ts`
+
+
+---
+
+Please note, installing packages. Use, the package tab or type `npm install <package>@<version>`
---
+++ index.tsx
@@ -0,0 +1,66 @@
+import { Hono } from 'hono'
+import { Database } from "bun:sqlite";
+import {
+  Content,
+  RequestList,
+  Home
+} from './html'
+
+const db = new Database("mydb.sqlite");
+db.run("CREATE TABLE IF NOT EXISTS requests (id INTEGER PRIMARY KEY AUTOINCREMENT, request TEXT)")
+
+const app = new Hono()
+
+// middleware
+
+app.use('*', async (c, next) => {
+  console.log(`[${c.req.method}] ${c.req.url}`)
+  
+  db.run("INSERT INTO requests (request) VALUES (?)", c.req.url)
+  await next()
+})
+
+// routes 
+
+app.get('/', (c) => {
+  const latestRequest = (db.query("SELECT * FROM requests ORDER BY id DESC").get())
+ const props = {
+    id: latestRequest.id,
+    request: latestRequest.request,
+    siteData: {
+      title: 'home',
+    },
+  }
+  return c.html(<Home {...props} />)
+})
+
+app.get('/requests', (c) => {
+  const latestRequests = (db.query("SELECT * FROM requests ORDER BY id DESC").all())
+  const props = {
+    requests: latestRequests,
+    siteData: {
+      title: 'all requests',
+    },
+  }
+  return c.html(<RequestList {...props} />)
+})
+
+
+app.get('/requests/:id', (c) => {
+  const id = c.req.param('id')
+  const latestRequest = (db.query("SELECT * FROM requests WHERE id = ?").get(id))
+  const props = {
+    id: latestRequest.id,
+    request: latestRequest.request,
+    siteData: {
+      title: 'request by id',
+    },
+  }
+  return c.html(<Content {...props} />)
+})
+
+export default {
+  port: 3000,
+  fetch: app.fetch,
+}
+    
